5650067ed67a61ac11e0487c20005568e49116b0
Snezana <snmitrov@cisco.com>
2026-03-23 16:47:02 +0100

    update tf state to cloud

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
+terraform {
+  cloud {
+    organization = "CX-Branch"
 
+    workspaces {
+      name = "bac-lab"
+    }
+  }
+}
 
 module "meraki" {
   source           = "github.com/netascode/terraform-meraki-nac-meraki?ref=v0.6.0"
