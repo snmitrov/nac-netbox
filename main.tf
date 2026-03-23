153eee04043862709231d9250fc6813fe1392f27
Snezana <snmitrov@cisco.com>
2026-03-23 18:48:06 +0100

    minor modifications

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ terraform {
   }
 }
 
+provider "meraki" {
+  retry_on_error_codes = [404, 400, 401]
+}
+
 module "meraki" {
   source           = "github.com/netascode/terraform-meraki-nac-meraki?ref=v0.6.0"
   yaml_directories = ["data"]
