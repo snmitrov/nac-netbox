e981e77a0e5b8e357a0179dcaaef7f596741f77d
Snezana <snmitrov@cisco.com>
2026-06-15 14:36:23 +0200

    update lab to v2.0.0

--- main.tf
+++ main.tf
@@ -11,6 +11,8 @@ provider "meraki" {
 }
 
 module "meraki" {
-  source           = "github.com/netascode/terraform-meraki-nac-meraki?ref=v0.6.0"
+  source           = "github.com/netascode/terraform-meraki-nac-meraki?ref=v0.9.0"
   yaml_directories = ["data"]
+  write_model_file = "merged_configuration.nac.yaml"
 }
+
--- workspaces/main.tf
+++
@@ -1,5 +0,0 @@
-module "model" {
-  source           = "github.com/netascode/terraform-meraki-nac-meraki/modules/model?ref=v0.6.0"
-  yaml_directories = ["../data"]
-  write_model_file = "merged_configuration.nac.yaml"
-}
